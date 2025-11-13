49b6d0421accd206ae9b4e66ac29307deb69326c
helrqxd <104747809+helrqxd@users.noreply.github.com>
2025-11-13 19:18:34 +0800

    Add files via upload

--- custom.js
+++ custom.js
@@ -23,3 +23,69 @@ $(function () {
         }
     }
 });
+
+$(function () {
+    $('#goahead').on('click', function () {
+        // Hide the goahead button
+        $('#goahead-container').fadeOut();
+
+        // Scale up the image
+        $('#castle-image').addClass('scale-up');
+
+        // After the animation, show the choices
+        setTimeout(function () {
+            $('#choice-container').css('display', 'flex').hide().fadeIn();
+            document.getElementById('shui-audio').play();
+        }, 1600); // A bit after the transform animation ends
+    });
+
+    // Placeholder for choice button clicks
+    $('.choice-btn').on('click', function () {
+        userChoices.choice = $(this).text();
+        // alert("你选择了: " + $(this).text());
+        // Here you can decide what to do next, for example, start the birthday animation
+        $('#choice-container').fadeOut();
+        $('#castle-image').fadeOut(function () {
+            $('.castle-container').remove();
+        });
+        $('.container').fadeIn('fast');
+    });
+
+    $('#play').on('click', function () {
+        $('#music-choice-container').css('display', 'flex').hide().fadeIn();
+    });
+
+    $('.music-choice-btn').on('click', function () {
+        let choice = $(this).text();
+        userChoices.music = choice;
+        let audio;
+        switch (choice) {
+            case '恋个爱':
+                audio = document.getElementById('gzqs-audio');
+                break;
+            case '许个愿':
+                audio = document.getElementById('fcl-audio');
+                break;
+            case '纯过个生日':
+                audio = document.getElementById('hbd-audio');
+                break;
+            case '拆个炸弹盒':
+                audio = document.getElementById('hdl-audio');
+                break;
+        }
+        if (audio) {
+            audio.play();
+        }
+        $('#music-choice-container').fadeOut();
+        $('#play').fadeOut('slow').delay(600).promise().done(function () {
+            $('#bannar_coming').fadeIn('slow');
+        });
+    });
+});
+
+const userChoices = {
+    choice: null,
+    light: null,
+    music: null,
+    balloons: null
+};
